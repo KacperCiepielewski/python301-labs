326fc6cce44f3c262a4a8a020fc892f1948e42ee
KacperCiepielewski <KacperCiepielewski@gmail.com>
2021-06-13 00:09:45 +0200

    05_03 lab solution added

--- 05_exceptions/05_03_check_for_ints.py
+++ 05_exceptions/05_03_check_for_ints.py
@@ -2,3 +2,11 @@
 # validity of the input type, and displays a message depending on whether
 # the input was an integer or not.
 # The script should keep prompting the user until they enter an integer.
+
+while True:
+    try:
+        provided_input = int(input("Please provide a valid integer: "))
+    except ValueError:
+        print("Regretfully that's not a valid integer - please try again!")
+    else:
+        break
